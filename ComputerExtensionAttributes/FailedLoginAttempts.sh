453a46c6d9066ffd74ce3a760e35de3a95eeec36
Trevor <trevoedwards@users.noreply.github.com>
2023-03-02 17:11:07 -0700

    Create FailedLoginAttempts.sh

---
+++ ComputerExtensionAttributes/FailedLoginAttempts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Extension Attribute to determine the number of failed login attempts during a specified duration
+
+searchDuration="24h"  # [--last &lt;num&gt;[m|h|d] ]
+
+failedLoginAttempts=$( /usr/bin/log show --last "${searchDuration}" --style syslog --predicate 'eventMessage contains "Failed to authenticate user"' | /usr/bin/wc -l | /usr/bin/tr -d ' ' )
+
+echo "&lt;result&gt;$failedLoginAttempts&lt;/result&gt;"
+
+exit 0
