a08e49c089a01cd7a697431c4acc65f97135264f
Trevor <trevoedwards@users.noreply.github.com>
2023-03-08 15:50:53 -0700

    Update LastReboot.sh

--- ComputerExtensionAttributes/LastReboot.sh
+++ ComputerExtensionAttributes/LastReboot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Shows when the device was last booted
+# Data Type: String
 
 echo -n "<result>$(date -jf '%s' "$(sysctl kern.boottime | awk -F'= |,' '{print $2}')" '+%Y-%m-%d %T')</result>"
 
