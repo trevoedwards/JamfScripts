4bba24ad8c88aea3bf22381f78050b75be54ab68
Trevor <trevoedwards@users.noreply.github.com>
2023-03-08 15:48:49 -0700

    Update AdditionalNetworkInformation.zsh

--- ComputerExtensionAttributes/AdditionalNetworkInformation.zsh
+++ ComputerExtensionAttributes/AdditionalNetworkInformation.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 # Jamf does not show all interfaces connected to a device. This will give you the IP & MAC of all connected NICs
+# data type: string
 
 data=$(/usr/libexec/PlistBuddy -x -c "Print :0:_items" /dev/stdin <<< $(system_profiler -xml SPNetworkDataType))
 
