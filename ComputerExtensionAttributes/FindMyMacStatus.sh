23a32f9b54ee2e3b816a26a4b057a74fb98e9885
Trevor <trevoedwards@users.noreply.github.com>
2023-06-05 14:10:54 -0700

    Create FindMyMacStatus.sh

---
+++ ComputerExtensionAttributes/FindMyMacStatus.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+#Displays whether Find My Mac and, by extension, Activation Lock are enabled/disabled on the computer. 
+
+fmmToken=$(/usr/sbin/nvram -x -p | /usr/bin/grep fmm-mobileme-token-FMM)
+
+if [ -z "$fmmToken" ];
+then echo "<result>Disabled</result>"
+else echo "<result>Enabled</result>"
+fi
