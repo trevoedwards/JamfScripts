65c93829af628d9f66fbb22cd9bca14223fb4ae2
Trevor <trevoedwards@users.noreply.github.com>
2025-05-05 20:43:24 -0700

    Update APP-InstallomatorStatus.sh
    Add reference to AppAutoPatch official doc

--- ComputerExtensionAttributes/APP-InstallomatorStatus.sh
+++ ComputerExtensionAttributes/APP-InstallomatorStatus.sh
@@ -5,6 +5,7 @@
 # Data Type: String
 # Purpose: Checks if Installomator is installed at default location for AppAutoPatch 3.x 
 # Installomator's default location is: /usr/local/Installomator/Installomator.sh
+# Find more AppAutoPatch related EA's here: https://github.com/App-Auto-Patch/App-Auto-Patch/tree/main/AAP-JamfProEAs
 
 # Path to expected Installomator script
 installomatorPath="/Library/Management/AppAutoPatch/Installomator/Installomator.sh"
