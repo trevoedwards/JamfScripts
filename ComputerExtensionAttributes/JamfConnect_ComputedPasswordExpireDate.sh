e250eb16d22ef32bd966cc4cfa24a267852eea3c
Trevor <trevoedwards@users.noreply.github.com>
2025-05-05 20:45:41 -0700

    Update and rename Jamf_Connect_ComputedPasswordExpireDate.sh to JamfConnect_ComputedPasswordExpireDate.sh
    Rename file and fix comments

--- ComputerExtensionAttributes/JamfConnect_ComputedPasswordExpireDate.sh
+++ ComputerExtensionAttributes/JamfConnect_ComputedPasswordExpireDate.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# Displays the value of the ComputedPasswordExpireDate attribute for the Jamf Connect user.
 #
 # Data Type: String
 # Input Type: Script
-#
+# Purpose: Displays the value of the ComputedPasswordExpireDate attribute for the Jamf Connect user.
 # Copyright (c) 2022 JAMF Software, LLC
-##### Script starts here #####
-
-#Get current signed in user
+#
+# Get current signed in user
 currentUser=$(ls -l /dev/console | awk '/ / { print $3 }')
 
-#com.jamf.connect.state.plist location
+# com.jamf.connect.state.plist location
 jamfConnectStateLocation=/Users/$currentUser/Library/Preferences/com.jamf.connect.state.plist
 
 ComputedPasswordExpireDate=$(/usr/libexec/PlistBuddy -c "Print :ComputedPasswordExpireDate" $jamfConnectStateLocation || echo "Does not exist")
