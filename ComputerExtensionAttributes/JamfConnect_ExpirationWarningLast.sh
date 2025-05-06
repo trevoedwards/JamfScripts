09f13dd01b2f66da8f3733f59e94326e46f009a4
Trevor <trevoedwards@users.noreply.github.com>
2025-05-05 20:46:43 -0700

    Update and rename Jamf_Connect_ExpirationWarningLast.sh to JamfConnect_ExpirationWarningLast.sh
    Rename file and fix comment

--- ComputerExtensionAttributes/JamfConnect_ExpirationWarningLast.sh
+++ ComputerExtensionAttributes/JamfConnect_ExpirationWarningLast.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-# Displays the date for when the last password expiration warning was displayed for the Jamf Connect user.
 #
 # Data Type: String
 # Input Type: Script
+# Purpose: Displays the date for when the last password expiration warning was displayed for the Jamf Connect user
 #
 # Copyright (c) 2022 JAMF Software, LLC
-##### Script starts here #####
 
-#Get current signed in user
+# Get current signed in user
 currentUser=$(ls -l /dev/console | awk '/ / { print $3 }')
 
-#com.jamf.connect.state.plist location
+# com.jamf.connect.state.plist location
 jamfConnectStateLocation=/Users/$currentUser/Library/Preferences/com.jamf.connect.state.plist
 
 ExpirationWarningLast=$(/usr/libexec/PlistBuddy -c "Print :ExpirationWarningLast" $jamfConnectStateLocation || echo "Does not exist")
